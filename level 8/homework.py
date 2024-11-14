511032469c2c8501093099f6693949b3e5966373
nikaasab229 <uzumakn693@gmail.com>
2024-11-14 12:07:45 +0400

    second commit

---
+++ level 8/homework.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 5
+num3 = 2
+num4 = 7
+num5 = 3
+print((num1 + num2) - (num3 * num4) / num5 ** num2 % num1)
+print((num1 * num3) + (num4 / num2) - num5 ** num3 % num1)
+print(num1 ** num4 % num2 + num3 * num5 - num2)
+print((num4 + num1) / num5 * num3 - num2 ** num3)
+print(num3 + num1 * num2 - num4 % num5 + num1)
+print(num1 - (num5 * num2) + (num3 ** num4) / num5 % num2)
+print(num2 + num1 * num5 / num3 - num4 ** num3)
+print(num1 * (num4 / num2) + num3 ** num5 - num2)
+print(num3 - (num1 ** num5) % num4 + num2 * num1)
+print((num4 + num2) * num3 - num1 ** num5 % num2)
