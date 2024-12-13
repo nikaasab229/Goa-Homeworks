66644f33c7cc2255b4e662147ee0079feb4874bd
nikaasab229 <uzumakn693@gmail.com>
2024-12-13 16:31:55 +0400

    commit

--- level 15/classwork/classwork.py
+++
@@ -1,6 +0,0 @@
-
-number = int(input("type number: "))
-sum_of_numbers = 0
-for i in range(number + 1):
-    sum_of_numbers += i  
-print(f"დიაპაზონის რიცხვების ჯამი არის: {sum_of_numbers}")
---
+++ level 20/classwork/homework.py
@@ -0,0 +1,18 @@
+
+elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+element1 = elements[0]
+element2 = elements[1]
+element3 = elements[2]
+element4 = elements[3]
+element5 = elements[4]
+element6 = elements[5]
+element7 = elements[6]
+element8 = elements[7]
+element9 = elements[8]
+element10 = elements[9]
+print(element1, element2, element3, element4, element5, element6, element7, element8, element9, element10)
+num1 = int(input("type first number: "))
+num2 = int(input("type second number: "))
+num3 = int(input("type third number: "))
+numbers_list = [num1, num2, num3]
+print("numbers list:", numbers_list)
