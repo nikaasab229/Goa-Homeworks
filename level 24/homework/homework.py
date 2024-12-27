f747bff56b5ce54d4b723ee23e49c2ae40b8afd6
nikaasab229 <uzumakn693@gmail.com>
2024-12-27 21:52:35 +0400

    commit

---
+++ level 24/homework/homework.py
@@ -0,0 +1,18 @@
+my_list = [10, 20, 30, 40, 50]
+first_element = my_list[0]
+print(first_element)
+last_element = my_list[-1]
+print(last_element)
+first_three_elements = my_list[:3]
+print(first_three_elements)
+my_string = "gamar.joba"
+last_five_characters = my_string[-5:]
+print(last_five_characters)
+reversed_string = my_string[::-1]
+print(reversed_string)
+every_third_element = my_list[::3]
+print(every_third_element)
+half1 = my_list[:len(my_list)//2]
+half2 = my_list[len(my_list)//2:]
+print(half1, half2)
+
