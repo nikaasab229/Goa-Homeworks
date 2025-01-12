4f8b63190796777294ba48cba11aa7c478f25ef9
nikaasab229 <uzumakn693@gmail.com>
2025-01-12 21:14:47 +0400

    commit

---
+++ level 26/homework/homework.py
@@ -0,0 +1,12 @@
+def sum_of_two_numbers(a, b):
+    return a + b
+def even_or_odd(n):
+    return "Even" if n % 2 == 0 else "Odd"
+def reverse_string(s):
+    return s[::-1]
+def find_maximum(numbers):
+    return max(numbers)
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
